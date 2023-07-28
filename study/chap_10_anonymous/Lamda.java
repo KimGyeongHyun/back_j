79a1993a89f2be90fe641ec974640d13754fd6fa
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2023-07-29 00:35:27 +0900

    chap 10

--- study/chap_10_anonymous/Lamda.java
+++ study/chap_10_anonymous/Lamda.java
@@ -6,9 +6,7 @@ import java.util.Comparator;
 
 // 함수형 인터페이스
 // 구현해야 할 추상 메소드가 하나만 정의된 인터페이스
-interface Math {
-    public int Calc(int first, int second);
-}
+interface Math {public int Calc(int first, int second);}
 
 public class Lamda {
     public static void main(String[] args) {
@@ -16,13 +14,53 @@ public class Lamda {
         // 기존 익명 구현 객체 활용 대신
         // 함수형 프로그래밍을 도입하면서 간단하게 표현 가능
 
+        // 특징
+        // 이름을 가질 필요가 없다 (익명 함수)
+        // 두 개 이상의 입력이 있는 함수는 최종적으로 1개의 입력만 받는
+        // 람다 대수로 단순화 될 수 있다. (Curing)
+
+        // 장점
+        // 코드의 간결성 - 불필요한 반복문의 삭제, 복잡한 식 단순화
+        // 지연연산 수행 - 불필요한 연산 최소화 (결과값이 필요할 때까지 계산을 늦추는 기법)
+        // 병렬처리 가능 - 멀티쓰레딩
+
+        // 단점
+        // 호출이 까다로움
+        // stream 사용 시 단순 반복문에서 사용할 경우 성능 저하
+        // 가독성을 떨어 뜨릴 수 있음
+
         // 기본 구조
         // (매개변수) -> {실행문}
+
+        // 정상적인 유형
+        // 함수 몸체가 단일 실행문 or return 문이라면 괄호 생략 가능
+        // () -> {}
+        // () -> 1
+        // () -> { return 1; }
+
+        // (int x) -> x+1
+        // (x) -> x+1
+        // x -> x+1
+        // (int x) -> { return x+1; }
+        // x -> { return x+1; }
+
+        // (int x, int y) -> x+y
+        // (x, y) -> x+y
+        // (x, y) -> { return x+y; }
+
+        // (String lam) -> lam.length()
+        // lam -> lam.length()
+        // (Thread lamT) -> { lamT.start(); }
+        // lamT -> { lamT.start(); }
+
+        // 잘못된 유형 선언된 type과 선언되지 않은 type을 같이 사용 할 수 없다.
+        // (x, int y) -> x+y
+        // (x, final y) -> x+y  
 
         // Comparator : compare 추상 메소드를 가지는 함수형 인터페이스
         // 람다식으로 compare 추상 메소드를 구현
 
-        // 기존 익명 클래스 구현 방식
+        // 1. 기존 익명 클래스 구현 방식
         Comparator<Integer> comp = new Comparator<Integer>() {
             // o1.compareTo(o2)
             // o1 이 크면 1, o2 이 크면 -1, 같으면 0 리턴
@@ -31,21 +69,25 @@ public class Lamda {
         };
         System.out.println(comp.compare(2, 1));
 
-        // 람다 방식
+        // 2. 람다 방식
         Comparator<Integer> comp2 = (o1, o2) -> (o1.compareTo(o2));
         System.out.println(comp2.compare(2, 1));
 
         // 익명 클래스로 배열 정렬
         List<Integer> l1 = new ArrayList<Integer>();
         l1.add(5); l1.add(3); l1.add(4); l1.add(2); l1.add(1);
-        Collections.sort(l1, comp);
+        for (Integer i : l1) System.out.print(i + ", ");
+        System.out.println();
+        Collections.sort(l1, comp);     // 정렬
         for (Integer i : l1) System.out.print(i + ", ");
         System.out.println();
 
         // 람다식으로 배열 정렬
         List<Integer> l2 = new ArrayList<Integer>();
         l2.add(5); l2.add(3); l2.add(4); l2.add(2); l2.add(1);
-        Collections.sort(l2, comp2);
+        for (Integer i : l2) System.out.print(i + ", ");
+        System.out.println();
+        Collections.sort(l2, comp2);    // 정렬
         for (Integer i : l2) System.out.print(i + ", ");
         System.out.println();
 
@@ -66,5 +108,12 @@ public class Lamda {
         System.out.println(plusLambda.Calc(4, 2));
         Math minusLambda = (first, second) -> first - second;
         System.out.println(minusLambda.Calc(4, 2));
+
+        // 스레드 구현
+        // Thread 는 Runnable 람다 함수가 인자로 전달되며
+        // start() 함수로 Runnable 람다 함수 실행
+        Runnable run = () -> System.out.println("run() method called");
+        Thread th = new Thread(run);
+        th.start();
     }
 }
--- study/chap_10_anonymous/StreamAPI.java
+++ study/chap_10_anonymous/StreamAPI.java
@@ -1,25 +1,333 @@
 package study.chap_10_anonymous;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.DoubleStream;
+import java.util.Random;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.OptionalInt;
+import java.util.Map;
+
+class Product {
+
+    int number; String name;
+
+    Product(int number, String name) {
+        this.number = number; this.name = name;
+    }
+
+    public String getName() {return name;}
+    public Integer getAmount() {return number;}
+}
 
 public class StreamAPI {
+    
     public static void main(String[] args) {
         // Stream
         // 다양한 데이터를 표준화된 방법으로 다루기 위한 라이브러리
+        // 람다 활용
+        // 배열, 컬렉션 인스턴스에 함수 여러 개를 조합하여
+        // 원하는 결과 필터링, 가공된 결과 얻을 수 있음
+        // 지연연산, 병렬처리 가능
+        // 데이터 변경 불가능, 1회용 연산
+        // 멀티스레드 코드 구현 없이 데이터를 투명하게 병렬로 처리 가능
         
         // stream() : 스트림 생성
-        // filter : 중간 연산 (스트림 변환) (연속 수행 가능)
-        // count : 최종 연산 (스트림 사용) (마지막에 한 번 사용 가능)
-
-        // 특징
-        // 데이터 변경 X
-        // 1회용
-        // 지연 연산 수행
-        // 병렬 실행 가능
+        // filter : 중간 연산 (스트림 변환, 가공) (연속 수행 가능)
+        // count : 최종 연산 (스트림 사용, 결과 획득) (마지막에 한 번 사용 가능)
+        // 전체 -> 맵핑 -> 필터링 1 -> 필터링 2 -> 결과 만들기 -> 결과물
 
         // 종류
         // Stream<T> : 범용 Stream
         // IntStream, LongStream, DoubleStream
 
-        IntStream.range(1, 11).filter(i -> i%2==0).forEach(System.out::println);
+        // =============================================================================
+        // 스트림 생성
+
+        // 배열 스트림 생성
+        String[] arr = new String[]{"a", "b", "c"};
+        Stream<String> stream1 = Arrays.stream(arr);
+        // 1~2 요소 [b, c]
+        Stream<String> streamOfArrayPart = Arrays.stream(arr, 1, 3);
+        
+        // 컬렉션 스트림 (Collection, List, Set) 생성
+        List<String> list = Arrays.asList("a", "b", "c:");
+        Stream<String> stream2 = list.stream();
+        Stream<String> parallelStream = list.parallelStream();
+
+        // Stream.builder()
+        // 스트림에 직접적으로 원하는 값 넣을 수 있음
+        // 마지막에 build 메소드로 스트림 리턴
+        // [Eric, Elena, Java]
+        Stream<String> builderStream = Stream.<String>builder()
+        .add("Eric").add("Elena").add("Java").build();
+
+        // Stream.generate()
+        // Supplier<T> 에 해당하는 람다로 값 넣을 수 있음
+        // 인자 없이 리턴값만 있는 함수형 인터페이스
+        // 최대 크기 지정 필요
+        // [el, el, el, el, el]
+        // 5개의 "gen" 이 들어간 스트림 생성
+        Stream<String> generatedStream = Stream.generate(() -> "gen").limit(5);
+
+        // Stream.iterate()
+        // 초기값과 람다를 이용해서 스트림에 들어갈 요소 만듦
+        // [30, 32, 34, 36, 38]
+        Stream<Integer> iteratedStream = Stream.iterate(30, n -> n + 2).limit(5);
+
+        // 기본 타입형 스트림 (int, long, double)
+        // 리스트, 배열 이용해서 스트림 생성
+        // range, rangeClosed 는 두 번째 인자인 종료지점 포함 여부 차이
+        IntStream intStream = IntStream.range(1, 5); // [1, 2, 3, 4]
+        LongStream longStream = LongStream.rangeClosed(1, 5); // [1, 2, 3, 4, 5]
+
+        // 제네릭을 사용하지 않기 때문에 불필요한 오토박싱이 일어나지 않음
+        // boxed 메소드로 박싱
+        Stream<Integer> boxedIntStream  = IntStream.range(1, 5).boxed();
+
+        // 난수 스트림 생성
+        DoubleStream doubles = new Random().doubles(3);
+
+        // 문자열 스트림
+        // 각 문자를 IntStream 으로 변환
+        // [83, 116, 114, 101, 97, 109]
+        // IntStream charsStream = "Stream".chars(5);
+
+        // 정규 표현식 이용해서 문자열 자르고 각 요소들로 스트림 생성
+        // [Eric, Elena, Java]
+        Stream<String> stringStream = Pattern.compile(", ").splitAsStream("Eric, Elena, Java");
+
+        // 파일 스트림
+        // 각 라인 스트링 타입의 스트림 생성
+        // Stream<String> lineStream = Files.lines(Paths.get("file.txt"), Charset.forName("UTF-8"));
+
+        // 스트림 연결
+        // [Java, Scala, Groovy, Python, Go, Swift]
+        Stream<String> stream3 = Stream.of("Java", "Scala", "Groovy");
+        Stream<String> stream4 = Stream.of("Python", "Go", "Swift");
+        Stream<String> concat = Stream.concat(stream3, stream4);
+
+        // 병렬 스트림
+        // 생성
+        // Product 는 클래스, productList 는 Product 객체 리스트
+        // Stream<Product> parallelStream = productList.parallelStream();
+
+        // 병렬 여부 확인
+        // parallelStream : 이전에 ["a", "b", "c"] 리스트를 병렬 스트림으로 생성해놓음
+        // 결과는 true 
+        boolean isParallel = parallelStream.isParallel();
+
+        // 예시
+        // boolean isMany = parallelStream
+        // .map(product -> product.getAmount() * 10)
+        // .anyMatch(amount -> amount > 200);
+
+        // 배열을 이용해서 병렬 스트림 생성
+        // Arrays.stream(arr).parallel();
+
+        // =============================================================================
+        // 가공하기 (중간 작업)
+        // 원하는 것만 뽑아냄
+        // 스트림 리턴이기 때문에 여러 작업 이어붙일 수 있음
+
+        List<String> names = Arrays.asList("Eric", "Elena", "Java");
+
+        // Filtering
+        // 요소들 하나씩 평가해서 걸러냄
+        // boolean 리턴하는 함수형 인터페이스가 평가식으로 들어감
+        // [Elena, Java]
+        Stream<String> stream = names.stream().filter(name -> name.contains("a"));
+
+        // Mapping
+        // 요소들 하나씩 특정 값으로 변환
+        // 값을 변환하기 위한 람다 인자로 받음
+        // [ERIC, ELENA, JAVA]
+        // :: 는 람다식에서 메소드 참조 표현할 때 쓰임
+        Stream<String> stream5 = names.stream().map(String::toUpperCase);
+
+        // Sorting
+        // 원래는 Collections.sort(List<T>, lambda) 로 List<T> 안의 요소 정렬
+        // 대신 아래 식으로 가비지 변수인 List<T> 를 만들지 않고 정렬된 배열 추출 가능
+        IntStream.of(14, 11, 20, 39, 23)
+        .sorted().boxed().collect(Collectors.toList());
+
+        List<String> lang = Arrays.asList("Java", "Scala", "Groovy", "Python", "Go", "Swift");
+        lang.stream().sorted().collect(Collectors.toList()); // 알파벳 순 정렬
+        lang.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()); // 반대 정렬
+        
+        // 문자열 길이 기준 정렬
+        lang.stream().sorted(Comparator.comparingInt(String::length)).
+        collect(Collectors.toList());
+        // 반대 정렬
+        lang.stream().sorted((s1, s2) -> s2.length() - s1.length()).
+        collect(Collectors.toList());
+
+        // Iterating
+        // 요소들 각각에 대해 특정 연산 수행
+        // peek : 리턴이 없는 함수형 인터페이스를 인자로 받음
+        // 특정 작업만 수행하고 결과에 영향을 미치지 않음
+        int sum = IntStream.of(1, 3, 5, 7, 9)
+        .peek(System.out::println).sum();
+
+        // =============================================================================
+        // 결과 만들기
+
+        // Calculating
+        // count : 갯수, sum() 총합
+        long count = IntStream.of(1, 3, 5, 7, 9).count();
+
+        // min, max 는 요소가 없으면 표현할 수 없으므로 Optional 을 이용해 리턴
+        OptionalInt min = IntStream.of(1, 3, 5, 7, 9).min();
+        OptionalInt max = IntStream.of().max();
+
+        // ifPresent() : 값을 가지고 있는지 확인 후 예외처리 (Optional)
+        // 즉, 요소가 없으면 그냥 넘어감
+        DoubleStream.of(1.1, 2.2, 3.3, 4.4, 5.5)
+        .average().ifPresent(System.out::println);
+        
+        // Reduction
+        // reduce() 메소드
+        // 총 세 가지의 파라미터 받을 수 있음
+        // accumulator : 각 요소를 처리하는 계산 로직
+        // identity : 계산을 위한 초기값, 내용 없어도 이 값은 리턴
+        // combiner : 병렬 스트림에서 나눠 계산한 결과를 합치는 로직
+
+        // 인자 1 개 (accumulator)
+        // 같은 타입의 인자 두 개를 받아 같은 타입의 결과를 반환하는 함수형 인터페이스 전달
+        // [1, 2, 3] 총합 리턴 6
+        OptionalInt reduced = IntStream.range(1, 4)     
+        .reduce((a, b) -> {return Integer.sum(a, b);});
+
+        // 인자 2 개 (identity, accumulator)
+        // 10 이 초기값, 모두 더하여 16
+        int reducedTwoParams = IntStream.range(1, 4)
+        .reduce(10, Integer::sum);
+
+        // 인자 3 개 (identity, accumulator, combiner)
+        // Combiner 는 병렬 처리 시 각자 다른 쓰레드에서 실행한 결과를
+        // 마지막에 합치는 단계 (병렬 스트림에서만 동작)
+        // 아래 코드에서는 병렬 스트림이 아니므로 Combiner 가 동작하지 않음 (16)
+        Integer reducedParams = Stream.of(1, 2, 3)
+        .reduce(10,
+                Integer::sum,
+                (a, b) -> {
+                    System.out.println("combiner was called");
+                    return a + b;
+                }
+        );
+        
+        // 병렬 스트림
+        // 10+1, 10+2, 10+3 의 총합이 리턴됨 (36)
+        Integer reducedParallel = Arrays.asList(1, 2, 3)
+        .parallelStream()
+        .reduce(10, 
+                Integer::sum,
+                (a, b) -> {
+                    System.out.println("combiner was called");
+                    return a + b;
+                });
+
+        reduced.ifPresent(System.out::println); // 6
+        System.out.println(reducedTwoParams);   // 16
+        System.out.println(reducedParams);      // 16
+        System.out.println(reducedParallel);    // 36
+
+        // Collecting
+        // Collectors 객체에서 자주 사용하는 인자 제공
+
+        List<Product> productList =
+        Arrays.asList(
+            new Product(23, "potatoes"),
+            new Product(14, "orange"),
+            new Product(13, "lemon"),
+            new Product(23, "bread"),
+            new Product(13, "sugar")
+        );
+
+        // [potatoes, orange, lemon, bread, sugar]
+        List<String> collectorCollection =
+        productList.stream()
+        .map(Product::getName)
+        .collect(Collectors.toList());
+        
+        // potatoesorangelemonbreadsugar
+        String listToString =
+        productList.stream()
+        .map(Product::getName)
+        .collect(Collectors.joining());
+        System.out.println(listToString);
+
+        // <potatoes, orange, lemon, bread, sugar>
+        String listToString2 =
+        productList.stream()
+        .map(Product::getName)
+        .collect(Collectors.joining(", ", "<", ">"));
+        System.out.println(listToString2);
+
+        // 17.2
+        Double averageAmount =
+        productList.stream()
+        .collect(Collectors.averagingInt(Product::getAmount));
+        System.out.println(averageAmount);
+
+        // 86
+        Integer summingAmount =
+        productList.stream()
+        .collect(Collectors.summingInt(Product::getAmount));
+        System.out.println(summingAmount);
+
+        // Collectors.groupingBy()
+        // 특정 조건으로 요소들을 그룹지을 수 있다
+
+        // Map 타입으로 묶음
+        Map<Integer, List<Product>> collectorMapOfLists =
+        productList.stream()
+        .collect(Collectors.groupingBy(Product::getAmount));
+
+        // Map 타입의 다양한 방법으로 묶는 방법이 있음
+
+        // Collector.of()
+        // 이 외에 필요한 로직이 있다면
+        // 직접 collector 로직을 만들어 Collector 생성
+        // reduce 와 동일한 파라미터 존재
+        Collector<Product, ?, LinkedList<Product>> toLinkedList = 
+        Collector.of(LinkedList::new, 
+            LinkedList::add, 
+            (first, second) -> {
+                first.addAll(second);
+                return first;
+        });
+
+        // 결과가 담긴 LinkedList 리턴
+        LinkedList<Product> linkedListOfPersons = 
+        productList.stream()
+        .collect(toLinkedList);
+
+        // Matching
+        // 람다 Predicate 받아 해당 조건 만족하는 요소 체크, 결과 리턴
+        // anyMatch() : 하나라도 조건을 만족하는 요소가 있는지
+        // allMatch() : 모두 조건을 만족하는지
+        // noneMatch() : 모두 조건을 만족하지 않는지
+        
+        boolean anyMatch = names.stream()
+        .anyMatch(name -> name.contains("a"));
+        boolean allMatch = names.stream()
+        .allMatch(name -> name.length() > 3);
+        boolean noneMatch = names.stream()
+        .noneMatch(name -> name.endsWith("s"));
+
+        // Iterating
+        // 요소를 돌면서 실행되는 최종 작업
+        // 보통 sout 메소드 넘겨 결과 출력에 사용
+        // peek 과는 중간, 최종 작업의 차이
+        names.stream().forEach(System.out::println);
     }
 }
+
+// 코드 참고 : https://futurecreator.github.io/2018/08/26/java-8-streams/
